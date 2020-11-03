b96ddf5de816e07c8f7dc39fc85663264fdde239
nt555 <nt.demo055@gmail.com>
2020-11-03 08:02:31 +0530

    added hill

--- sketch.js
+++ sketch.js
@@ -5,6 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
+var hill;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -17,6 +18,8 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    hill = new Ground (180,305,300,170);
+
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -49,6 +52,7 @@ function draw(){
     pig1.display();
     log1.display();
 
+    hill.display();
     box3.display();
     box4.display();
     pig3.display();
